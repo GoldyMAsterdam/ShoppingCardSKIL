c54d120983764422245e73def4330d58e3058a56
Tycho Boom <38436@ma-web.nl>
2025-05-19 12:36:52 +0200

    Finished most of the website

--- main.js
+++ main.js
@@ -1,6 +1,52 @@
 
 let cartCount = 0;
 
+let cards = [
+    [
+        "Kawasaki H2R",
+        
+        2500,
+
+        "19ZX1000Y_201GY3DRF1CG_A_001.png",
+
+        "Kawasaki H2R Beschrijving",
+
+        "Nee"
+    ],
+    [
+        "Yamaha",
+
+        799.99,
+
+        "s11728_yamaha_bike_isolated_on_white_background_17b769a0-b1bc-4ed2-a783-cb3def840146_1-photoroom.png",
+
+        "Yamaha Beschrijving",
+
+        "Nee"
+    ],
+    [
+        "BMW S1K",
+
+        2999.99,
+
+        "bmw-transparent-S1000RR_1445x.webp",
+
+        "BMW S1K Beschrijving",
+        
+        "Ja"
+    ]
+];
+
+const container = document.getElementById('product-container');
+
+for (let i = 0; i < cards.length; i++) {
+    let beschikbaar = cards[i][4] === "Ja";
+    let disabledClass = beschikbaar ? ""
+    :
+    "product__button--disabled";
+    let disabledAttr = beschikbaar ? "" : "disabled";
+}
+
 function AddToCart(button) {
     if (button.classList.contains('product__button--disabled') || button.disabled)
     {
